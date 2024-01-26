191f11e7d081d64869420a72f23899d327e47b24
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-01-26 11:55:08 +0000

    s

--- App.2.py
+++ App.2.py
@@ -2,7 +2,6 @@ import pandas as pd
 import unicodedata
 import streamlit as st
 
-
 def importar_datos(posicion):
     filename = f"df_{posicion}_medias.csv"
     df = pd.read_csv(filename)
@@ -25,22 +24,23 @@ liga_seleccionada = st.selectbox("¿Qué liga deseas ver?", ligas)
 if liga_seleccionada != "Todas las ligas":
     Data = Data[Data['League'] == liga_seleccionada]
 
-# Define las características para cada posición
+# Define las características para cada posición con su respectivo peso
 caracteristicas_por_posicion = {
-    "Delanteros": ["Big chances missed","Goals","Headed goals"],
-    "Mediocampista": ["Interceptions", "Goals","Assists"],
-    "Defensas": ["Tackles", "Interceptions"],
-    "Porteros": ["Aerial duels won %", "Penalties faced", "Saves"]
+    "Delanteros": {"Big chances missed": -1, "Goals": 1, "Headed goals": 1},
+    "Mediocampista": {"Interceptions": 1, "Goals": 1, "Assists": 1},
+    "Defensas": {"Tackles": 1, "Interceptions": 1},
+    "Porteros": {"Aerial duels won %": 1, "Penalties faced": -1, "Saves": 1}
 }
 
 # Obtén las características para la posición seleccionada
 caracteristicas = caracteristicas_por_posicion[opcion]
 
 # Convierte las columnas a tipo numérico para evitar el error
-Data[caracteristicas] = Data[caracteristicas].apply(pd.to_numeric, errors='coerce')
+for caracteristica in caracteristicas:
+    Data[caracteristica] = pd.to_numeric(Data[caracteristica], errors='coerce')
 
-# Calcula el score total para cada jugador
-Data['Score total'] = Data[caracteristicas].sum(axis=1)
+# Calcula el score total para cada jugador teniendo en cuenta el peso de cada característica
+Data['Score total'] = sum(Data[caracteristica] * peso for caracteristica, peso in caracteristicas.items())
 
 # Ordena el dataframe por el score total y toma los primeros 10
 Data = Data.sort_values(by='Score total', ascending=False)
@@ -49,4 +49,5 @@ top_jugadores = Data.head(10)
 # Muestra los resultados
 st.write("Los 10 mejores jugadores según el score total son:")
 st.write(top_jugadores[['Name', 'Score total', 'League']])
+
 
