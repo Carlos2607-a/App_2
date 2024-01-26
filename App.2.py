47e3993dcbd9441e53c1d739a50663316ab6ec0c
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-01-26 13:44:45 +0000

    s

--- App.2.py
+++ App.2.py
@@ -15,18 +15,17 @@ opcion = st.selectbox("¿Qué posición deseas consultar?", opciones)
 
 Data = importar_datos(opcion)
 Data_copy = Data.copy()
-
-# Obtén todas las ligas únicas en el DataFrame
+# Obtén todas las ligas únicas en el Data_copyframe
 ligas = ["Todas las ligas"] + Data_copy['League'].unique().tolist()
 liga_seleccionada = st.selectbox("¿Qué liga deseas ver?", ligas)
 
-# Filtra el DataFrame por la liga seleccionada, a menos que el usuario haya seleccionado "Todas las ligas"
+# Filtra el Data_copyframe por la liga seleccionada, a menos que el usuario haya seleccionado "Todas las ligas"
 if liga_seleccionada != "Todas las ligas":
     Data_copy = Data_copy[Data_copy['League'] == liga_seleccionada]
 
 # Define las características para cada posición con su respectivo peso
 caracteristicas_por_posicion = {
-    "Delanteros": {'Goals':2.5, 'Headed goals':1.4, 'Total shots':1.4, 'Assists':1.2, 'Big chances created':2, 'Was fouled':1.20,'Set piece conversion %':0.35, 'Accurate passes %':0.6, 'Successful dribbles %':0.5, 'Total duels won %':0.3,'Big chances missed':-0.15},
+    "Delanteros": {'Goals':2.55, 'Headed goals':1.2, 'Total shots':1, 'Assists':1, 'Big chances created':2.6, 'Was fouled':1,'Set piece conversion %':0.55, 'Accurate passes %':0.6, 'Successful dribbles %':0.5, 'Total duels won %':0.3,'Big chances missed':-0.25},
     "Mediocampista": {"Interceptions": 1, "Goals": 1, "Assists": 1},
     "Defensas": {"Tackles": 1, "Interceptions": 1},
     "Porteros": {"Aerial duels won %": 1, "Penalties faced": -1, "Saves": 1}
@@ -40,9 +39,9 @@ for caracteristica in caracteristicas:
     Data_copy[caracteristica] = pd.to_numeric(Data_copy[caracteristica], errors='coerce')
 
 # Calcula el score total para cada jugador teniendo en cuenta el peso de cada característica
-Data_copy['Score total'] = sum(Data_copy[caracteristica] * peso for caracteristica, peso in caracteristicas.items() if peso > 0) - sum(Data_copy[caracteristica] * abs(peso) for caracteristica, peso in caracteristicas.items() if peso < 0)
+Data_copy['Score total'] = sum(Data_copy[caracteristica] * peso for caracteristica, peso in caracteristicas.items())
 
-# Ordena el DataFrame por el score total y toma los primeros 10
+# Ordena el Data_copyframe por el score total y toma los primeros 10
 Data_copy = Data_copy.sort_values(by='Score total', ascending=False)
 top_jugadores = Data_copy.head(10)
 
