f2223e308d3e948900d10d15f36a44c377dbedb8
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-02-14 21:58:22 +0000

    d

--- App3.py
+++ App3.py
@@ -27,19 +27,19 @@ if liga_seleccionada != "Todas las ligas":
 
 # Define las características para cada posición con su respectivo peso
 caracteristicas_por_posicion =  {
-    "Delanteros": {'Goals':2.55, 'Headed goals':1.2, 'Total shots':1, 'Assists':1, 'Big chances created':2.6, 'Was fouled':1,'Set piece conversion %':0.55, 'Accurate passes %':0.6, 'Successful dribbles %':0.5, 'Total duels won %':0.3,'Big chances missed':-0.25},
-    "Mediocampista": {'Set piece conversion %':0.45,'Dribbled past':0.8,'Assists':1.5,'Big chances created':1.8,'Accurate final third passes':1,'Was fouled':0.5,'Tackles':1.5,'Fouls':0.35,'Total shots':1,'Accurate long balls %':1,'Accurate passes %':0.75,'Successful dribbles %':0.75,'Total passes':1.5,'Interceptions':1.55,'Goals':1.5,'Total duels won %':1.8},
-    "Defensas": {'Penalty committed': -0.5,'Interceptions': 0.55,'Errors lead to goal': -0.65,'Total passes': 0.65,'Tackles': 1,'Goals conceded inside the box': -0.4,'Aerial duels won %': 0.9,'Total duels won %': 1,'Accurate passes %': 0.5,'Fouls': -0.4,'Dribbled past': -0.6},
-    "Porteros": {'Penalties saved': 0.5,'Saves': 2,'Errors lead to goal': -1,'Clean sheets': 3,'Aerial duels won %': 0.75,'Total duels won %': 1.5,'Penalty committed': -0.5,'Goals conceded inside the box': 1,'Goals conceded outside the box': -0.75,'Total passes': 0.5,'Accurate long balls %': 0.5,'Accurate passes %': 0.5}
+    # Tus características aquí
 }
 
 # Calcula el score total para cada jugador teniendo en cuenta el peso de cada característica
+resultados = []
 for posicion, caracteristicas in caracteristicas_por_posicion.items():
-    Data_posicion = Data_copy[Data_copy['Posicion'] == posicion]
+    Data_posicion = Data_copy[Data_copy['Posicion'] == posicion].copy()
     for caracteristica in caracteristicas:
         Data_posicion[caracteristica] = pd.to_numeric(Data_posicion[caracteristica], errors='coerce')
     Data_posicion['Score total'] = sum(Data_posicion[caracteristica] * peso for caracteristica, peso in caracteristicas.items() if peso > 0) - sum(Data_posicion[caracteristica] * abs(peso) for caracteristica, peso in caracteristicas.items() if peso < 0)
-    Data_copy[Data_copy['Posicion'] == posicion] = Data_posicion
+    resultados.append(Data_posicion)
+
+Data_copy = pd.concat(resultados)
 
 # Ordena el dataframe por el score total y selecciona los mejores jugadores por posición
 mejores_jugadores = pd.concat([
